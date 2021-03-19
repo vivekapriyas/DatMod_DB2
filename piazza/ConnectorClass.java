bab0702c4caf8756e208a37488ddc9e4bf16439c
Viveka Priya Simhan <vivekaps@stud.ntnu.no>
2021-03-19 12:01:15 +0100

    endret på noen filnavn osv

--- piazza/ConnectorClass.java
+++ piazza/ConnectorClass.java
@@ -1,27 +1,28 @@
-package piazza;
 import java.sql.*;
+public abstract class ConnectorClass {
 
+    Connection conn;
 
-
-public class ConnectorClass {
-    public static void main(String[] args) {
-
+    public ConnectorClass() {
         try {
-            //Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db2", "vivekaps","Piki3lapskaus");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/prosjekt2", "root","RighteouSsql3211");
             PreparedStatement stmt = conn.prepareStatement("Select  * from course");
             ResultSet rs = stmt.executeQuery();
-                while(rs.next())
-                {
-                    System.out.println(rs.getString(1));
-                }
-            conn.close();
-        } 
-        catch(Exception e)
-            {
-                System.out.println(e);
-            }
+                     while(rs.next())
+                     {
+                         System.out.println(rs.getString(1));
+                     }
+                 conn.close();
+             } 
+             catch(Exception e)
+                 {
+                     System.out.println("Hvis dette vises kompilerer koden i alle fall");
+                 }
+        
     }
+
+
 
 
 
