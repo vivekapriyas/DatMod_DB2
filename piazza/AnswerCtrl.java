19f35710246841b2627dec6d566548f32c82156a
dandanfre <daniefre@stud.ntnu.no>
2021-03-22 12:06:38 +0100

    blabla

--- piazza/AnswerCtrl.java
+++ piazza/AnswerCtrl.java
@@ -5,8 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;  
 
-
-//Se gjennom koden for evt. feil
 //Mangler logikk for å kontrollere eksistensen av courseID
 
 
@@ -15,42 +13,60 @@ public class AnswerCtrl extends ConnectorClass{
 
     public int postID;
     public String content;
-    public String username;
 
-    public AnswerCtrl(int postid, String content, String username){
+
+    public AnswerCtrl(/*int postid, String content*/){
         connect();
-        this.postID = postid;
-        this.content = content;
-        this.username = username;  //usikker på om dette er riktig måte å hente username på
+        //this.postID = postid;
+        //this.content = content;
     }
 //balbablalba
-    public void createPost(){
+    public void createAnswer(int postID, String content){
+        this.postID = postID;
+        this.content = content;
+       final String insertion = "INSERT INTO instructorsanswer VALUES(?,?)";
         try{
-            LocalDate date = LocalDate.now();
-            //String date_string = date;
-            String colorCode = "UPDATE post SET colorCode = 'yellow' WHERE postID = ?"; 
-            String insertion = "INSERT INTO instructorsanswer VALUES(?,?)";
-            String answeredByI = "INSERT INTO answeredbyinstructor VALUES(?,?,?)";
             PreparedStatement regStmt = conn.prepareStatement(insertion);
-            PreparedStatement regStmt1 = conn.prepareStatement(answeredByI);
-            PreparedStatement regStmt2 = conn.prepareStatement(colorCode);
             regStmt.setInt(1,this.postID);   //regstmt2 setter inn i IA
             regStmt.setString(2,this.content);
             regStmt.execute();
-            regStmt1.setInt(1,this.postID);  //regStmt1 setter inn i ABI
-            regStmt1.setString(2,this.username);
-            regStmt1.setObject(3,date);
-            regStmt1.execute();         
-            regStmt2.executeUpdate(colorCode); //oppdaterer colorCode i post
-
         }
         catch(SQLException e){
             System.out.println("***DB error during preparation of insert into instructoranswer***");
         }   
+
+
+        LocalDate date = LocalDate.now();
+        String username = "stein@ntnu.no";  //"instruktør"
+       // String emne = "TDT4145";
+        String answeredByI = "INSERT INTO answeredbyinstructor VALUES(?,?,?)";
+        try {
+            PreparedStatement regStmt1 = conn.prepareStatement(answeredByI);
+            regStmt1.setInt(1,this.postID);  
+            regStmt1.setString(2,username);
+            regStmt1.setObject(3,date);
+            regStmt1.execute(); 
+            
+        } 
+        catch (Exception e) {
+            System.out.println("***DB error during preparation of insert into answeredbyinstructor***");
+        }
+
+        String updateColor = "UPDATE post SET post.colorCode = 'yellow' WHERE post.postID = ?";
+        try {
+            PreparedStatement regStmt2 = conn.prepareStatement(updateColor);
+            regStmt2.setInt(1,this.postID);        
+            regStmt2.executeUpdate();
+        } 
+        catch (Exception e) {
+            System.out.println("**DB error during updating color");
+            e.printStackTrace();
+        }
+
     }
     
     public static void main(String[] args) {
-        AnswerCtrl test = new AnswerCtrl(1,"Dette er et svar","abc@ntnu.no");
-        test.createPost();
+        AnswerCtrl test = new AnswerCtrl();
+        test.createAnswer(2,"Dette er et svar");
     }
 }
